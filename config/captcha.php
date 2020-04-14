7b3c91ed8830a4a456e91d9638ae548e945eec24
Henry Lee <henryleeworld@gmail.com>
2020-04-14 19:36:49 +0800

    feat:升級至 laravel 7

--- config/captcha.php
+++ config/captcha.php
@@ -3,16 +3,16 @@
 return [
     'characters' => ['2', '3', '4', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'm', 'n', 'p', 'q', 'r', 't', 'u', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'M', 'N', 'P', 'Q', 'R', 'T', 'U', 'X', 'Y', 'Z'],
     'default' => [
-        'length' => 5,
+        'length' => 9,
         'width' => 120,
-        'height' => 35,
+        'height' => 36,
         'quality' => 90,
-        'math' => true,
+        'math' => false,
     ],
     'math' => [
         'length' => 9,
         'width' => 120,
-        'height' => 35,
+        'height' => 36,
         'quality' => 90,
         'math' => true,
     ],
@@ -20,7 +20,7 @@ return [
     'flat' => [
         'length' => 6,
         'width' => 160,
-        'height' => 35,
+        'height' => 46,
         'quality' => 90,
         'lines' => 6,
         'bgImage' => false,
@@ -36,7 +36,7 @@ return [
     'inverse' => [
         'length' => 5,
         'width' => 120,
-        'height' => 35,
+        'height' => 36,
         'quality' => 90,
         'sensitive' => true,
         'angle' => 12,
